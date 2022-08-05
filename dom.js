948dd4a65b6e64531a93bac5eec655d9a114ef27
bintexhimself <binutirihabib@gmail.com>
2022-08-05 20:14:31 +0100

    Simple Javascript todo list

---
+++ dom.js
@@ -0,0 +1,96 @@
+let count =0;
+for(count=0; count<=10; count++){
+console.log(count);
+}
+
+
+
+const calculateAge= (num)=>{
+    console.log(2022-num);
+}
+
+calculateAge(1985);
+
+
+
+let toDoArr = ['Taking a walk with the president', 'Reading at night for exam', 'Doing the house shores'];
+
+const toDoItems = document.getElementById('todo-items');
+    toDoItems.innerHTML = ''; //resetting the list
+
+toDoArr.forEach((e)=>{
+    const item = document.createElement('p');
+        toDoItems.appendChild(item);
+        item.innerHTML = e;
+        item.classList.add('item');
+        const btn = document.createElement('button');
+        btn.innerText = 'delete';
+        btn.classList.add('close');
+        item.appendChild(btn);
+        document.getElementById('tagname').value = '';
+})
+
+//Deleting todo items//
+
+const btn = document.querySelectorAll('.close');
+
+btn.forEach((button)=>{
+    button.onclick =()=>{
+        button.parentElement.style.display = 'none';
+        toDoArr.splice(button, 1);
+        console.log(toDoArr);
+    }
+
+});
+
+//Adding a todolist item//
+document.getElementById('btn').addEventListener('click', (e) =>{
+
+    const inputVal = document.getElementById('tagname').value;
+
+
+    if(inputVal ==''){
+        alert('You have not enter any task!!');
+        document.getElementById('todo-items').innerHTML = '';
+        e.preventDefault();
+
+    } else if (!isNaN(inputVal)) {
+        alert('Sorry! you can only add a text!');
+        e.preventDefault();
+
+    }  else {
+        toDoArr.push(inputVal);
+        console.log(toDoArr);
+        const item = document.createElement('p');
+        toDoItems.appendChild(item);
+        item.innerHTML = inputVal;
+        item.classList.add('item');
+        const btn = document.createElement('button');
+        btn.classList.add('close');
+        btn.innerText = 'delete';
+        item.appendChild(btn);
+        document.getElementById('tagname').value = '';
+        localStorage.setItem("todoList", JSON.stringify(toDoArr));
+    }
+     
+    e.preventDefault();
+   
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
